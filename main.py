1b0e20d25ec188e62b2d9aaaf703b07c82b13727
Nazar Shmatko <cutlass900@gmail.com>
2024-06-27 15:14:37 -0400

    switched to local server

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import requests
 import os
 import streamlit as st
+from requests.auth import HTTPBasicAuth
+import urllib3
+
+# Suppress only the single InsecureRequestWarning from urllib3 needed
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 X_API_KEY = os.getenv("X_API_KEY")
+username = os.getenv("USER_NAME")
+password = os.getenv("PASSWORD")
 
 st.set_page_config(layout="wide")
 st.title("Relevant Sources Demo V0.4")
@@ -11,15 +18,16 @@ if st.button("Get Sources"):
     if not st.session_state.get('document'):
         st.write('Please input your document first.')
     else:
-        with st.spinner("Processing, this may take up to a minute..."):
-            url = "https://api.gptzero.me/v2/relevant_sources/text"
-            headers = {'Content-Type': 'application/json',
-                       'x-api-key': X_API_KEY}
+        with st.spinner("Processing, this may take up to a 2 minutes..."):
+            url = "https://e125-99-209-159-2.ngrok-free.app/relevant_sources"
+            headers = {'Content-Type': 'application/json'}
             data = {
                 "text": st.session_state['document']
             }
             try:
-                response = requests.post(url, headers=headers, json=data)
+                response = requests.post(url, headers=headers, json=data,
+                                        auth=HTTPBasicAuth(username, password),
+                                        verify=False)
                 response.raise_for_status() 
                 st.session_state['results'] = response.json().get('results', [])
             except requests.exceptions.HTTPError as http_err:
@@ -30,7 +38,7 @@ if st.button("Get Sources"):
 doc_col, analysis_col = st.columns(2)
 
 with doc_col:
-    st.session_state['document'] = st.text_area("Place your document here", height=1000, max_chars=2000)
+    st.session_state['document'] = st.text_area("Place your document here", height=500, max_chars=1000)
 
 with analysis_col:
     if 'results' in st.session_state:
